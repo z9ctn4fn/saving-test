0024b5ea71d85524602195709c12c26f13e7262d
Evan Kok <z9ctn4fn@gmail.com>
2022-04-12 13:28:23 -0700

    first commit

---
+++ Assets/CounterManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CounterManager : MonoBehaviour
+{
+    public Text text;
+    private static float number;
+    private string numstring;
+    void Start()
+    {
+        numstring = number.ToString();
+    }
+    public void Increase()
+    {
+        number++;
+    }
+    public void Decrease()
+    {
+        number--;
+    }
+    public void Update()
+    {
+        numstring = number.ToString();
+        text.text = numstring;
+    }
+}
